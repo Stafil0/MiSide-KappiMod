6fb3393e1e11e0692d7ed7caeb4a73e42a2717f1
Alexander <37180898+MrSago@users.noreply.github.com>
2026-07-25 16:22:08 +0500

    fix: error windows patch - update positioning logic

--- Patches/Rng/ErrorWindowsPatch.cs
+++ Patches/Rng/ErrorWindowsPatch.cs
@@ -1,7 +1,9 @@
 using HarmonyLib;
 using KappiMod.Logging;
 using KappiMod.Mods;
+using KappiMod.Patches.Core;
 using KappiMod.UI.Internal.EventDisplay;
+using UnityEngine;
 #if ML
 using Il2Cpp;
 #elif BIE
@@ -11,26 +13,42 @@ using BepInEx.IL2CPP;
 namespace KappiMod.Patches.Rng;
 
 [HarmonyPatch]
-internal sealed class ErrorWindowsPatch : ScopedRandomPatch
+internal sealed class ErrorWindowsPatch : IPatch
 {
-    public override string Id => "com.kappimod.errorwindows";
-    public override string Name => "Error Windows Patch";
-    public override string Description =>
-        "Error window always jumps to the first slot";
+    public string Id => "com.kappimod.errorwindows";
+    public string Name => "Error Windows Patch";
+    public string Description => "Error windows always stay in the first slot";
+
+    private static readonly Vector3 _firstPosition = new(-6.692f, 1.341f, -1.094f);
+
+    private readonly HarmonyLib.Harmony _harmony;
+
+    public ErrorWindowsPatch()
+    {
+        _harmony = new(Id);
+        _harmony.PatchAll(typeof(ErrorWindowsPatch));
+    }
+
+    public void Dispose()
+    {
+        _harmony.UnpatchSelf();
+    }
 
     [HarmonyPrefix]
-    [HarmonyPatch(typeof(Location11_ErrorWindows), nameof(Location11_ErrorWindows.ClickOkStage2))]
-    private static void BeforeClickOkStage2(out bool __state) => __state = DisableRandom();
-
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(Location11_ErrorWindows), nameof(Location11_ErrorWindows.ClickOkStage2))]
-    private static void AfterClickOkStage2(bool __state) => RestoreRandom(__state);
-
-    [HarmonyPostfix]
     [HarmonyPatch(typeof(Location11_ErrorWindows), nameof(Location11_ErrorWindows.Play))]
-    private static void AfterPlay()
+    private static void AfterPlay(Location11_ErrorWindows __instance)
     {
-        const string MESSAGE = "Ghostly: error OK stays in first slot";
+        __instance.errorWindowClone.transform.localPosition = _firstPosition;
+        __instance.errorWindowOKPosition.transform.localPosition = _firstPosition;
+        __instance.errorWindowReady.transform.localPosition = _firstPosition;
+
+        __instance.positions = new(new Vector3[15]);
+        for (int i = 0; i < __instance.positions.Length; ++i)
+        {
+            __instance.positions[i] = _firstPosition;
+        }
+
+        const string MESSAGE = "Ghostly: error windows stay in the first slot";
         EventManager.ShowEvent(new($"{nameof(BlessRng)}: {MESSAGE}"));
         KappiLogger.Log(MESSAGE);
     }
